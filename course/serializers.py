2ad3a83b4fd433c29629c530d66b3698928f4d7b
suvorovjr <pglavan1998@mail.ru>
2024-03-09 15:20:53 +0300

    modify CourseSerializer

--- course/serializers.py
+++ course/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
+from lesson.serializers import LessonSerializers
 from course.models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializers(source='lesson', many=True)
 
     class Meta:
         model = Course
