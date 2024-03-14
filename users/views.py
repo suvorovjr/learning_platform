b0f093eba7e0b8b4da6dc6f3fff89e811aa9fa98
suvorovjr <pglavan1998@mail.ru>
2024-03-14 13:26:51 +0300

    add User CRUD

--- users/views.py
+++ users/views.py
@@ -1,8 +1,27 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework import generics
-from users.serializers import PaymentSerializer
-from users.models import Payment
+from users.serializers import PaymentSerializer, UserSerializer
+from users.models import Payment, User
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = Payment.objects.all()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = Payment.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = Payment.objects.all()
 
 
 class PaymentListAPIView(generics.ListAPIView):
