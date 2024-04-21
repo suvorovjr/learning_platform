e0e2e552c450ba5bb40bfb9ed3bd77ef583e8a59
suvorovjr <pglavan1998@mail.ru>
2024-04-21 18:45:57 +0300

    add changes in course model

--- course/models.py
+++ course/models.py
@@ -8,6 +8,7 @@ class Course(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название')
     imagine = models.ImageField(upload_to='courses/', verbose_name='Изображение', **NULLABLE)
     description = models.TextField(verbose_name='Описание')
+    amount = models.PositiveIntegerField(verbose_name='Цена курса')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, verbose_name='Автор урока', **NULLABLE)
 
     def __str__(self):
